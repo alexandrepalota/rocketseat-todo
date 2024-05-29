ade171849ddf083dce8cde6d9428ca7c866a056f
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2024-05-29 10:28:01 -0300

    feat: header component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+import { View } from "react-native";
+
+export function Home() {
+    return (
+        <View>
+            <Header />
+        </View>
+    );
+}
