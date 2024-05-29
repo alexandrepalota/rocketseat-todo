6ca37b775715675a4df0dd995011ae3eea9bb3a2
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2024-05-29 11:03:43 -0300

    feat: input form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,24 @@
 import { Header } from "../../components/Header";
-import { View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from './styles'
 
 export function Home() {
     return (
-        <View>
+        <View style={styles.container}>
             <Header />
+
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Adicione uma nova tarefa"
+                    placeholderTextColor='#808080'
+                />
+                <TouchableOpacity style={styles.button}>
+                    <Image source={require('../../../assets/plus.png')}/>
+                </TouchableOpacity>
+            </View>
+
+            
         </View>
     );
 }
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1A1A1A',
+        flex: 1
+    },
+    form: {
+        height: 56,
+        marginHorizontal: 24,
+        marginTop: -27,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    input: {
+        padding: 16,
+        borderRadius: 6,
+        color: '#F2F2F2',
+        borderColor: '#0D0D0D',
+        borderWidth: 2,
+        backgroundColor: '#262626',
+        flex: 1,
+        marginEnd: 4
+    },
+    button: {
+        width: 52,
+        height: 52,
+        backgroundColor: '#1E6F9F',
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {}
+});
