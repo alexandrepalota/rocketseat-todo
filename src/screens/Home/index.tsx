bfb9ca377b4a330a19400b7e3cf3d0f29f611931
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2024-05-29 12:59:51 -0300

    feat: task list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,23 @@
 import { Header } from "../../components/Header";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from './styles'
 import { Counter } from "../../components/Counter";
 import { Task } from "../../components/Task";
+import { EmptyMessage } from "../../components/EmptyMessage";
+
+const tasks = [
+    {id: 1, text: 'Lavar a louça', done: false},
+    {id: 2, text: 'Levar o lixo na rua', done: false},
+    {id: 3, text: 'Comprar açúcar', done: false},
+    {id: 4, text: 'Lavar o carro', done: false},
+    {id: 5, text: 'Cortar a grama', done: false},
+    {id: 6, text: 'Gardar as roupas', done: false},
+    {id: 7, text: 'Pagar a conta de luz', done: false},
+    {id: 8, text: 'Marcar dentista', done: false}
+]
 
 export function Home() {
+
     return (
         <View style={styles.container}>
             <Header />
@@ -25,10 +38,15 @@ export function Home() {
                 <Counter counterName="Concluídas" count={2}/>
             </View>
 
-            <Task done={true} text='Lavar a louça'/>
-            <Task done={true} text='Lavar a louça'/>
-            <Task done={true} text='Lavar a louça'/>
-
+            <FlatList
+                data={tasks}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({item}) => (
+                    <Task text={item.text} done={item.done}/>
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => <EmptyMessage />}
+            />
             
         </View>
     );
