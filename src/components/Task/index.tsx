6d216b0f85ef8a094a4edb2c9bab0bb17417472b
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2024-05-29 13:40:42 -0300

    feat: app states and splash screen

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -8,19 +8,21 @@ import trashImage from './../../../assets/trash.png';
 type Props = {
     done: boolean;
     text: string;
+    onDelete: (text: string) => void;
+    onToggleDone: (text: string) => void;
 }
 
 export function Task(props: Props) {
     return (
         <View style={styles.contanier}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => props.onToggleDone(props.text)}>
                 <Image style={styles.check} source={props.done ? checkedImage : uncheckedImage} />
             </TouchableOpacity>
             <Text
                 style={props.done ? styles.doneText : styles.text}
                 numberOfLines={2}
                 ellipsizeMode='tail'>{props.text}</Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => props.onDelete(props.text)}>
                 <Image style={styles.trash} source={trashImage} />
             </TouchableOpacity>
         </View>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,65 @@
 import { Header } from "../../components/Header";
-import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from './styles'
 import { Counter } from "../../components/Counter";
 import { Task } from "../../components/Task";
 import { EmptyMessage } from "../../components/EmptyMessage";
+import { useState } from "react";
 
-const tasks = [
-    {id: 1, text: 'Lavar a louça', done: false},
-    {id: 2, text: 'Levar o lixo na rua', done: false},
-    {id: 3, text: 'Comprar açúcar', done: false},
-    {id: 4, text: 'Lavar o carro', done: false},
-    {id: 5, text: 'Cortar a grama', done: false},
-    {id: 6, text: 'Gardar as roupas', done: false},
-    {id: 7, text: 'Pagar a conta de luz', done: false},
-    {id: 8, text: 'Marcar dentista', done: false}
-]
+type Task = {
+    text: string;
+    done: boolean;
+}
 
 export function Home() {
+
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [done, setDone] = useState(0);
+    const [created, setCreated] = useState(0);
+    const [taskInput, setTaskInput] = useState('');
+
+    function handleTaskAdd() {
+        const newTask = taskInput.trim()
+        if (!newTask) {
+            return Alert.alert('O texto da tarefa é obrigatório');
+        }
+        if (tasks.filter(t => t.text === newTask).length) {
+            return Alert.alert(`A tarefa ${newTask} já está na sua lista`);
+        }
+        setTasks(prevState => [...prevState, {text: newTask, done: false}]);
+        setCreated(created + 1)
+        setTaskInput('');
+    }
+
+    function handleTaskDelete(text: string) {
+        Alert.alert('Excluir', `Excluir a tarefa ${text}?`, [
+            {
+                text: 'Sim',
+                onPress: () => {
+                    const taskToDelete = tasks.filter(t => t.text)[0];
+                    setTasks(prevState => prevState.filter(t => t.text !== text));
+                    setCreated(created - 1);
+                    if (taskToDelete.done) {
+                        setDone(done - 1);
+                    }
+                }
+            },
+            {
+                text: 'Não',
+                style: 'cancel'
+            }
+        ]);
+    }
+
+    function handleDoneToggle(text: string) {
+        const updatedTasks = tasks.forEach(t => {
+            if (t.text === text) {
+                t.done = !t.done;
+                setDone(t.done ? done + 1 : done - 1);
+                return;
+            }
+        });
+    }
 
     return (
         <View style={styles.container}>
@@ -27,22 +70,24 @@ export function Home() {
                     style={styles.input}
                     placeholder="Adicione uma nova tarefa"
                     placeholderTextColor='#808080'
+                    onChangeText={setTaskInput}
+                    value={taskInput}
                 />
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={handleTaskAdd}>
                     <Image source={require('../../../assets/plus.png')}/>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.counters}>
-                <Counter counterName="Criadas" count={2}/>
-                <Counter counterName="Concluídas" count={2}/>
+                <Counter counterName="Criadas" count={created}/>
+                <Counter counterName="Concluídas" count={done}/>
             </View>
 
             <FlatList
                 data={tasks}
-                keyExtractor={item => item.id.toString()}
+                keyExtractor={item => item.text}
                 renderItem={({item}) => (
-                    <Task text={item.text} done={item.done}/>
+                    <Task text={item.text} done={item.done} onDelete={handleTaskDelete} onToggleDone={handleDoneToggle}/>
                 )}
                 showsVerticalScrollIndicator={false}
                 ListEmptyComponent={() => <EmptyMessage />}
